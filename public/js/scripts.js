cbb5f0c801322bf8df08cb8e58090856d9557d12
Kyle Wiebers <kyle@ballyhoo.it>
2012-09-30 18:49:35 -0500

    Addee backdoor for ballybot, tested and worked'

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,13 @@
 var socket = io.connect();
 
-  socket.on('error', function (reason){
-    console.error('Unable to connect Socket.IO', reason);
-  });
+socket.on('error', function (reason){
+  console.error('Unable to connect Socket.IO', reason);
+});
 
-  socket.on('connect', function (){
-    console.info('successfully established a working connection');
-    if($('.chat .chat-box').length == 0) {
-      
-    }
-  });
+socket.on('connect', function (){
+  console.info('successfully established a working connection');
+  
+});
 
 socket.on('message:send', function(data) {
 	$('#chat-container').append('<p><strong>'+data.nickname+': </strong>'+data.msg+'</p>');
