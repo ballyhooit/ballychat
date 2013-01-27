581a0537501e5a9e4e0ac2e6544ea2c6ded915c4
Kyle Wiebers <kyle@ballyhoo.it>
2013-01-27 12:56:56 -0600

    Removed all chat UI pieces, created only socket.io hub for chat

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express')
   , http = require('http')
-  , passport = require('passport')
-  , RedisStore = require('connect-redis')(express)
   , fs = require('fs')
   , nconf = exports.nconf = require('nconf')
   , kn = require('knox')
@@ -29,29 +27,15 @@ var knox = exports.knox = kn.createClient({
 var redisUrl = exports.redisUrl = require('url').parse(nconf.get('REDISTOGO_URL'))
    , redisAuth = exports.redisAuth = redisUrl.auth.split(':');
 
-var sessionStore = exports.sessionStore = new RedisStore({host: redisUrl.hostname, port: redisUrl.port, db: redisAuth[0], pass: redisAuth[1]});
-
-require('./strategy');
-
 var app = exports.app = express();
 
 app.configure(function() {
   app.set('port', nconf.get('PORT'));
   app.set('view engine', 'jade'); 
-  app.set('views', __dirname + '/views/themes/default');
   app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
   app.use(express.cookieParser('ThisIsASecret'));
-  app.use(express.session({
-    key: nconf.get('SESSION_KEY'),
-    store: sessionStore
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
 });
-
-require('./routes');
 
 exports.server = http.createServer(app).listen(app.get('port'), function() {
   winston.info('Ballyhoo started on port '+ app.get('port'));
